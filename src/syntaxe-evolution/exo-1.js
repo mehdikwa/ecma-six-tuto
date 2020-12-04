53eb6eeba89e84107b039987d9dc2b0ed6ca7d74
Mehdi Merabti <mhd.merabti@gmail.com>
2020-12-04 14:33:30 +0000

    refactor: another way to build map object

--- src/syntaxe-evolution/exo-1.js
+++ src/syntaxe-evolution/exo-1.js
@@ -7,10 +7,35 @@ const stringLetters = (string) => {
     }, {})
 }
 
-function countLetters4(str){
+function countLettersUsingMap(str) {
+    let count = 1;
+    const myMap = new Map();
+
+    for (const char of Array.from(str)) {
+        const charInMap = myMap.has(char);
+        if (charInMap) {
+            const value = myMap.get(char);
+            myMap.set(char, value+1);
+        } else {
+            myMap.set(char, count);
+        }
+    }
+    console.log(myMap);
+}
+
+function countLettersUsingReduce(str){
     return [...str].reduce((accumulator, currentValue) => Object.assign(accumulator, {
         ...({[currentValue]: !accumulator[currentValue] ? 1 : ++accumulator[currentValue]})
     }), {} );
+}
+
+const countLettersUsingReduceOther = (string) => {
+    return string.split("").reduce((accumulator, currentValue) => {
+        accumulator.hasOwnProperty(currentValue) ?
+            accumulator[currentValue]++ :
+            accumulator[currentValue] = 1
+        return accumulator
+    }, {})
 }
 
 const isPresent = (string, target) => {
